4b6c58fca5cb067070fdc26cff4016f5ad88eae0
eliezer bauer <lazerbauer@gmail.com>
2023-05-08 14:43:05 +0100

    variabls-if-alert

---
+++ variable.js
@@ -0,0 +1,12 @@
+let greeting = "Welcome";
+let MyName = "Eliezer";
+let message = greeting + " " + MyName;
+alert(message);
+
+let age = 25;
+age *= 2;
+if (age > 80) {
+  alert("your age is over 40");
+} else {
+  alert("your age is less than 40");
+}
